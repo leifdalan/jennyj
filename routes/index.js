a7f3f1a16fa72f3d556df00c9cc89d86be94fa4a
Leif Dalan <leifdalan@gmail.com>
2014-03-22 11:45:53 -0700

    added cloudinary.

--- routes/index.js
+++ routes/index.js
@@ -9,102 +9,194 @@ var express = require('express'),
   Q = require('q'),
   crypto = require('crypto'),
   monk = require('monk'),
+  keys = process.env.NODE_ENV || require('../local-config');
   AWSKEY = process.env.AWSKEY || require('../local-config').AWSKEY,
   AWSSECRET = process.env.AWSSECRET || require('../local-config').AWSSECRET,
   BUCKET = process.env.BUCKET || require('../local-config').BUCKET,
   mongoURI = process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||
-    'localhost:27017/jennyjtest';
+    'localhost:27017/jennyjtest',
+  cloudinary = require('cloudinary')
 console.log(mongoURI);
 var db = monk(mongoURI);
   db_images = db.get('images');
 
 console.log(db);
+console.log(keys);
+cloudinary.config({ 
+  cloud_name: keys.CLOUD_NAME, 
+  api_key: keys.CLOUD_API_KEY, 
+  api_secret: keys.CLOUD_SECRET 
+});
 
+var record = {};
+
+var sizes = [
+  {
+    width: 500,
+    crop : 'fill',
+    gravity : 'face'
+  },
+  {
+    width: 1000,
+    crop : 'fill',
+    gravity : 'face',
+    format : 'webp' 
+  },
+  {
+    width: 1000,
+    crop : 'fill',
+    gravity : 'face',
+    format : 'webp' 
+  },
+  {
+    width: 100,
+    height: 100,
+    crop : 'fill',
+    gravity : 'face',
+    format : 'webp' 
+  }
+];
 
 
 exports.balls = function(req, res) {
-  var sizes = [
-      2000,
-      1000,
-      500,
-      320,
-      100
-    ],
-    deferredArray = [],
-    record = {};
+    var deferredArray = [];
+    
 
   if (req.files.displayImage.length) {
     for (var i = 0; i < req.files.displayImage.length; i++) {
-      handleUpload(req.files.displayImage[i]);
+      handleUpload(req.files.displayImage[i]).then(function() {
+        console.log(i + ' job done.');
+      });
+
     };
   } else {
-    handleUpload(req.files.displayImage)  
+    handleUpload(req.files.displayImage).then(function(response) {
+      console.log(response);
+      console.log('DONEEEEEE!!!!!!!!');
+    })
   }
   
 
   function handleUpload(file) {
+    var jobDeferred = Q.defer();
     fs.readFile(file.path, function (err, data) {
       var newPath = __parentDir + "/tmp/" + file.originalFilename;
-      function resizeAndPush(args) {
-
-        var thumbPath = __parentDir + '/tmp/' + args.id + '_' + args.thumbSize + '_' + file.originalFilename;
-        imageMagick(newPath)
-          .resize(args.thumbSize)
-          .write(thumbPath, function(err) {
-            
-            var amazonPath = '/tmp/' + args.id + '_' + args.thumbSize + '_' + file.originalFilename
-            console.log(amazonPath);
-            var client = knox.createClient({
-              key : AWSKEY,
-              secret : AWSSECRET,
-              bucket : BUCKET
-
-            });
-            var putting = client.putFile(thumbPath, amazonPath, function(err, s3res) {
-              console.log('err:');
-              console.log(err);
-              if (!s3res) return args.deferred.reject(err);
-              else s3res.resume(); 
-              record[args.id]['s_' + args.thumbSize] = amazonPath;
-              args.deferred.resolve(record);
-              console.log(record);
-            });
-
-            putting.on('progress', function(written) {
-              console.log(written);
-              
-            })
-            putting.on('error', function(error) {
-              console.log(error);
-
-            })
-        });//imageMagick
-      }
+     
 
       fs.writeFile(newPath, data, function (err) {
 
         var id = crypto.randomBytes(4).toString('hex'); 
         record[id] = {};
-        for (var i = 0; i < sizes.length; i++) {
-          var deferred = Q.defer();
+
+          
           var args = {
             id : id,
-            deferred : deferred,
-            thumbSize : sizes[i]
+            jobDeferred : jobDeferred,
+            filename : file.originalFilename,
+            file : file,
+            sizes : sizes
           }
           
           resizeAndPush(args);
 
-          deferredArray.push(args.deferred.promise);
-        };//for sizes
-        Q.allSettled(deferredArray).then(function(responseArray) {
-          console.log(responseArray);
-          res.end();
-          db_images.insert(record);
-        });
       });//writeFile
     });//readFile
+    return jobDeferred.promise
   }//handleUpoad
 
 }//balls
+
+function resizeAndPush(args) {
+    console.log(args.sizes);
+    var resizeDeferredArray = [];
+
+    cloudinary.uploader.upload(
+      args.file.path,
+      function(result) { 
+        console.log('result------------------');
+        console.log(result);
+
+        for (var i = 0; i < result.eager.length; i++) {
+          var resizeDeferred = Q.defer();
+          resizeDeferredArray.push(resizeDeferred.promise);
+          result.eager[i]
+          var tmpfile = fs.createWriteStream(__parentDir + '/tmp/' + result.eager[i].width + args.file.originalFilename);
+          console.log('writing');
+          console.log(tmpfile);
+          getFileFromCloudinAry({
+            url : result.eager[i].url,
+            tmpfile : tmpfile,
+            resizeDeferred : resizeDeferred,
+            id : args.id,
+            size : '' + result.eager[i].width + 'x' + result.eager[i].height,
+            name : args.file.originalFilename
+          })
+          Q.allSettled(resizeDeferredArray).then(function(responseArray) {
+            //console.log(responseArray);
+            args.jobDeferred.resolve(responseArray);
+            res.end();
+            //db_images.insert(record);
+          });
+
+        };
+      }, 
+      {
+        public_id: 'sample_id', 
+        crop: 'limit',
+        width: 2000,
+        height: 2000,
+        eager: args.sizes,                                     
+        tags: ['special', 'for_homepage']
+      }      
+    )
+  
+}
+
+function getFileFromCloudinAry(args) {
+  var request = http.get(args.url, function(response) {
+    response.pipe(args.tmpfile);
+    args.tmpfile.on('finish', function() {
+      args.tmpfile.close();
+      console.log(args.tmpfile);
+      var amazonPath = '/tmp/' + args.id + '_' + args.size + '_' + args.name
+      pushToS3({
+        tmpfile : args.tmpfile,
+        amazonPath : amazonPath,
+        resizeDeferred : args.resizeDeferred,
+        id : args.id,
+        size : args.size
+      });              
+    })
+  });
+}
+
+function pushToS3(args) {
+  var client = knox.createClient({
+    key : AWSKEY,
+    secret : AWSSECRET,
+    bucket : BUCKET
+
+  });
+  var putting = client.putFile(
+    args.tmpfile.path, 
+    args.amazonPath, 
+    function(err, s3res) {
+      console.log('err:');
+      console.log(err);
+      if (!s3res) return args.resizeDeferred.reject(err);
+      else s3res.resume(); 
+      record[args.id]['s_' + args.size] = args.amazonPath;
+      args.resizeDeferred.resolve(record);
+      console.log(record);
+  });
+
+  putting.on('progress', function(written) {
+    console.log(written);
+    
+  })
+  putting.on('error', function(error) {
+    console.log(error);
+
+  })
+}
